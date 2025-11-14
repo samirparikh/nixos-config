67a7a05a75a5d36d41e038f4a59daa94b2e53e02
Samir Parikh <siparikh@gmail.com>
2025-11-14 18:07:44 -0500

    enable pipewire; sound still doesn't work

--- modules/services.nix
+++ modules/services.nix
@@ -8,4 +8,12 @@
 
   # enable ifuse to connect to iPhone
   services.usbmuxd.enable = true;
+
+  # enable pipewire
+  services.pipewire.enable = true;
+  services.pipewire.alsa.enable = true;
+  services.pipewire.alsa.support32Bit = true;
+  services.pipewire.pulse.enable = true;
+  security.rtkit.enable = true;
+
 }
