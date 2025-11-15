a9cd8761d73b29006d7c94ba2120ba3a7f2663ab
Samir Parikh <siparikh@gmail.com>
2025-11-14 19:36:00 -0500

    update home.nix for git and firefox config

--- home.nix
+++ home.nix
@@ -47,6 +47,28 @@
     };
   };
 
+#  programs.firefox = {
+#    enable = true;
+#    profiles.samir = {
+#      extensions = {
+#        packages = with pkgs.nur.repos.rycee.firefox-addons; [
+#          # Add your extensions here. Some popular ones:
+#          ublock-origin
+#          tree-style-tab
+#          web-archives
+#          vimium
+#          # You can find more at: https://nur.nix-community.org/repos/rycee/
+#        ];
+#      };
+#      
+#      settings = {
+#        # Optional: Firefox settings
+#        "browser.startup.homepage" = "https://nixos.org";
+#        "browser.search.defaultenginename" = "DuckDuckGo";
+#      };
+#    };
+#  };
+
   programs.firefox = {
     enable = true;
     profiles.samir = {
@@ -65,7 +87,22 @@
         # Optional: Firefox settings
         "browser.startup.homepage" = "https://nixos.org";
         "browser.search.defaultenginename" = "DuckDuckGo";
+
+        # Additional settings to ensure extensions work
+        # "extensions.autoDisableScopes" = 0;
+        # "extensions.enabledScopes" = 15;
+        
+        # Required to enable userChrome.css
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
       };
+      
+      # Add userChrome.css
+      userChrome = ''
+        /* hides the native tabs */
+        #tabbrowser-tabs {
+          visibility: collapse !important;
+        }
+      '';
     };
   };
 
