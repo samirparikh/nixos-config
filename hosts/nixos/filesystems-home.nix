22da19c4d9e9ed412ab093a3ccd849a1240614cf
Samir Parikh <siparikh@gmail.com>
2025-11-22 10:19:28 -0500

    add new filesystem mount points

--- hosts/nixos/filesystems-home.nix
+++ hosts/nixos/filesystems-home.nix
@@ -1,10 +1,40 @@
-{ config, lib, pkgs, ... }:
+# nixos-config/hosts/nixos/filesystems-home.nix
+{ config, pkgs, ... }:
 
 {
-  fileSystems."/home" = {
+  fileSystems."/home/samir/Desktop" = {
     device = "/dev/disk/by-uuid/f4aa8ad6-e4af-4796-969a-6b2fae9aad15";
     fsType = "btrfs";
-    options = [ "subvol=@home" ];
+    options = [ "subvol=@home-Desktop" ];
   };
 
+  fileSystems."/home/samir/Documents" = {
+    device = "/dev/disk/by-uuid/f4aa8ad6-e4af-4796-969a-6b2fae9aad15";
+    fsType = "btrfs";
+    options = [ "subvol=@home-Documents" ];
+  };
+
+  fileSystems."/home/samir/Music" = {
+    device = "/dev/disk/by-uuid/f4aa8ad6-e4af-4796-969a-6b2fae9aad15";
+    fsType = "btrfs";
+    options = [ "subvol=@home-Music" ];
+  };
+
+  fileSystems."/home/samir/Pictures" = {
+    device = "/dev/disk/by-uuid/f4aa8ad6-e4af-4796-969a-6b2fae9aad15";
+    fsType = "btrfs";
+    options = [ "subvol=@home-Pictures" ];
+  };
+
+  fileSystems."/home/samir/Videos" = {
+    device = "/dev/disk/by-uuid/f4aa8ad6-e4af-4796-969a-6b2fae9aad15";
+    fsType = "btrfs";
+    options = [ "subvol=@home-Videos" ];
+  };
+
+  fileSystems."/home/samir/sites" = {
+    device = "/dev/disk/by-uuid/f4aa8ad6-e4af-4796-969a-6b2fae9aad15";
+    fsType = "btrfs";
+    options = [ "subvol=@home-sites" ];
+  };
 }
