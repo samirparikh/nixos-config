2eaaf9566e6d95d71081e77b738115d312d50bd0
Samir Parikh <siparikh@gmail.com>
2025-11-26 10:34:31 -0500

    remove commented out lines from flake.nix

--- flake.nix
+++ flake.nix
@@ -30,26 +30,11 @@
         modules = [
           ./hosts/nixos
 
-          # ---- Locale + command-not-found fixes ----
           ({ config, lib, ... }:
           {
-#            i18n = {
-#              defaultLocale = "en_US.UTF-8";
-#          
-#              extraLocaleSettings = {
-#                LC_TIME = lib.mkForce "en_GB.UTF-8";
-#              };
-#          
-#              supportedLocales = [
-#                "en_US.UTF-8/UTF-8"
-#                "en_GB.UTF-8/UTF-8"
-#              ];
-#            };
-          
             programs.command-not-found.enable = false;
             programs.nix-index.enable = true;
           })
-          # -------------------------------------------
 
           # home-manager integration
           home-manager.nixosModules.home-manager
