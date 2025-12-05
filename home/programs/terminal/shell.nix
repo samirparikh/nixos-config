0a9236b7d1e92633b869f861e45cfdf39de29912
Samir Parikh <siparikh@gmail.com>
2025-12-05 11:05:31 -0500

    update fish shell aliases

--- home/programs/terminal/shell.nix
+++ home/programs/terminal/shell.nix
@@ -1,33 +1,21 @@
 { config, pkgs, ... }:
 
 {
-#  programs.bash = {
-#    enable = true;
-#    
-#    shellAliases = {
-#      ll = "ls -alh";
-#      update = "sudo nixos-rebuild switch --flake ~/.config/nixos#\$(hostname)";
-#      upgrade = "sudo nixos-rebuild switch --upgrade --flake ~/.config/nixos#\$(hostname)";
-#    };
-#
-#    bashrcExtra = ''
-#      # Custom bash configuration
-#      export EDITOR=vim
-#    '';
-#  };
-
   programs.fish = {
     enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      la = "ls -a";
+      home = "cd ~/nixos-config/ || exit";
+      butane = ''
+        podman run --rm --interactive --security-opt label=disable --volume "''${PWD}:/pwd" --workdir /pwd quay.io/coreos/butane:release
+      '';
+      coreos-installer = ''
+        podman run --pull=always --rm --interactive --security-opt label=disable --volume "''${PWD}:/pwd" --workdir /pwd quay.io/coreos/coreos-installer:release
+      '';
+      ignition-validate = ''
+        podman run --rm --interactive --security-opt label=disable --volume "''${PWD}:/pwd" --workdir /pwd quay.io/coreos/ignition-validate:release
+      '';
+    };
   };
-
-#  programs.starship = {
-#    enable = false;
-#    settings = {
-#      add_newline = true;
-#      character = {
-#        success_symbol = "[➜](bold green)";
-#        error_symbol = "[➜](bold red)";
-#      };
-#    };
-#  };
 }
