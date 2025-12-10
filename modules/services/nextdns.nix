4e5882a9847e6d17a35c3a9a4a02946350b504f5
Samir Parikh <siparikh@gmail.com>
2025-12-10 17:37:20 -0500

    use AdGuard Home for DNS

--- modules/services/nextdns.nix
+++ modules/services/nextdns.nix
@@ -4,7 +4,8 @@
   environment.systemPackages = with pkgs; [ nextdns ];
 
   services.nextdns = {
-    enable = true;
+    # Set this back to true to enable NextDNS
+    enable = false;
     arguments = [ 
       "-config-file"
       config.sops.secrets.nextdns_config.path
@@ -13,9 +14,23 @@
     ];
   };
 
+# Uncomment these lines to enable NextDNS
+#  networking = {
+#    nameservers = [
+#      "45.90.28.110"
+#      "45.90.30.110"
+#    ];
+#  };
+
+# Comment out these lines to disable AdGuard Home
   networking = {
-    nameservers = [
-      "45.90.28.110"
-      "45.90.30.110" ];
+    nameservers = [ "192.168.1.181" ];
+    networkmanager.dns = "none";
+    resolvconf.useLocalResolver = false;
   };
+  
+  environment.etc."resolv.conf".text = ''
+    nameserver 192.168.1.181
+  '';
+# End AdGuard Home configuration
 }
