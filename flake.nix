f0fc59b655660978ac8f5e3699e44e3f35c1a365
Samir Parikh <siparikh@gmail.com>
2025-12-13 10:15:54 -0500

    revert flake back to 25.05 release

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Samir's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     # home-manager, used for managing user configuration
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
