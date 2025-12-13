da0fc376c9d8a43f623ca2450d37b04eb3599dea
Samir Parikh <siparikh@gmail.com>
2025-12-13 10:29:20 -0500

    update git.nix to 25.11 settings

--- home/programs/terminal/git.nix
+++ home/programs/terminal/git.nix
@@ -3,23 +3,21 @@
 {
   programs.git = {
     enable = true;
-    userName = "Samir Parikh";
-    userEmail = "siparikh@gmail.com";
-    
-    extraConfig = {
+
+    settings = {
+      user = {
+        name = "Samir Parikh";
+        email = "siparikh@gmail.com";
+      };
+
       init.defaultBranch = "main";
       core.editor = "vim";
       diff.tool = "vimdiff";
       difftool.prompt = false;
-    };
 
-    aliases = {
-      history = "log --all --oneline --decorate";
-      # st = "status";
-      # co = "checkout";
-      # br = "branch";
-      # ci = "commit";
-      # unstage = "reset HEAD --";
+      alias = {
+        history = "log --all --oneline --decorate";
+      };
     };
   };
 }
