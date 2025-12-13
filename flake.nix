ee41091a676303edb63f870e7777e2cab9fd8e2f
Samir Parikh <siparikh@gmail.com>
2025-12-13 10:07:00 -0500

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 
     # home-manager, used for managing user configuration
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
