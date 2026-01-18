05ed2b0724abc136882b1ff4718d29df9b232bfa
Samir Parikh <siparikh@gmail.com>
2026-01-18 14:13:04 -0500

    update daily and weekly btrbk timer start time

--- modules/services/btrbk.nix
+++ modules/services/btrbk.nix
@@ -36,7 +36,7 @@
     # Daily backups: Desktop and Documents
     # -------------------------------------------------------------------------
     daily = {
-      onCalendar = "daily";
+      onCalendar = "*-*-* 01:00:00";
       settings = {
         timestamp_format = "long";
         ssh_identity = "/root/.ssh/btrbk_ed25519";
@@ -74,7 +74,7 @@
     # Weekly backups: Music and sites
     # -------------------------------------------------------------------------
     weekly = {
-      onCalendar = "weekly";
+      onCalendar = "Mon *-*-* 13:00:00";
       settings = {
         timestamp_format = "long";
         ssh_identity = "/root/.ssh/btrbk_ed25519";
