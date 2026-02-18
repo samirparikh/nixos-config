18b88ea0bead76d9c4d746af708272a7c9f3ac8f
Samir Parikh <siparikh@gmail.com>
2026-02-17 19:02:02 -0500

    move zed settings into separate json file

--- home/programs/terminal/zed/default.nix
+++ home/programs/terminal/zed/default.nix
@@ -4,14 +4,6 @@
   programs.zed-editor = {
     enable = true;
     extensions = [ "nix" ];
-    userSettings = {
-      theme = {
-        mode = "system";
-        dark = "One Dark";
-        light = "One Light";
-      };
-      hour_format = "hour24";
-      vim_mode = true;
-    };
+    userSettings = builtins.fromJSON (builtins.readFile ./settings.json);
   };
 }
