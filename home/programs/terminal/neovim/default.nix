0fe13d51d052204a844d16aad9fa19861182fcca
Samir Parikh <siparikh@gmail.com>
2026-02-27 15:49:43 -0500

    refactor neovim configuration to simplify things

--- home/programs/terminal/neovim/default.nix
+++ home/programs/terminal/neovim/default.nix
@@ -1,11 +1,7 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, ... }:
 
 let
-  # Nix store paths injected as vim globals so init.lua stays portable.
-  # On non-NixOS systems, set these globals another way or rely on $PATH.
   nixPaths = ''
-    vim.g.nix_omnisharp_bin   = "${pkgs.omnisharp-roslyn}/bin/OmniSharp"
-    vim.g.nix_netcoredbg_bin  = "${pkgs.netcoredbg}/bin/netcoredbg"
     vim.g.nix_fsautocomplete_bin = "${pkgs.fsautocomplete}/bin/fsautocomplete"
   '';
 in
@@ -18,72 +14,28 @@ in
     vimdiffAlias = true;
 
     extraPackages = with pkgs; [
-      # .NET
-      omnisharp-roslyn
-      netcoredbg
-      csharpier
-      dotnet-sdk_8
       fsautocomplete
-
-      # General
+      dotnet-sdk_10
       ripgrep
-      fd
-      tree-sitter
-      nodejs_22
     ];
 
     plugins = with pkgs.vimPlugins; [
-      # Core
-      plenary-nvim
-
-      # Theme
       catppuccin-nvim
-
-      # Status line
-      lualine-nvim
-
-      # File explorer
-      nvim-web-devicons
-      nvim-tree-lua
-
-      # Treesitter
-      nvim-treesitter.withAllGrammars
-
-      # Telescope
-      telescope-fzf-native-nvim
-      telescope-nvim
-
-      # Completion
-      cmp-nvim-lsp
-      cmp-buffer
-      cmp-path
-      cmp-cmdline
-      luasnip
-      cmp_luasnip
-      friendly-snippets
-      lspkind-nvim
-      nvim-cmp
-
-      # Copilot
-      copilot-lua
-      copilot-cmp
-
-      # F# (Ionide-vim)
       Ionide-vim
-
-      # Git
-      gitsigns-nvim
-
-      # Quality of life
-      comment-nvim
-      nvim-autopairs
-      which-key-nvim
-      indent-blankline-nvim
-
-      # DAP (debugging)
-      nvim-dap
-      nvim-dap-ui
-      nvim-nio
+      lualine-nvim
+      (nvim-treesitter.withPlugins (parsers: [
+        parsers.fsharp
+        parsers.lua
+        parsers.vim
+        parsers.vimdoc
+        parsers.query
+        parsers.nix
+        parsers.bash
+        parsers.json
+        parsers.yaml
+        parsers.markdown
+        parsers.markdown_inline
+      ]))
     ];
 
     extraLuaConfig = nixPaths + builtins.readFile ./init.lua;
