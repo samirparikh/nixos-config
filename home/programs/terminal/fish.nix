41b5902062b86e8d54ae666902eebdbdb512a6a8
Samir Parikh <siparikh@gmail.com>
2026-07-05 18:30:53 -0400

    update fish shell alias for cses

--- home/programs/terminal/fish.nix
+++ home/programs/terminal/fish.nix
@@ -8,7 +8,7 @@
       la = "ls -a";
       home = "cd ~ || exit";
       nixos = "cd /home/samir/nixos-config/ || exit";
-      # cses = "cd /home/samir/Documents/programming/csharp/cses/ || exit";
+      cses = "cd /home/samir/Documents/programming/clang/cses2/ || exit";
     };
     shellAbbrs = {
       gcc = "gcc -std=c99 -O2 -Wall -Wextra";
